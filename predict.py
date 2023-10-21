27a9ed00b01941909f5343df17b3ec0d5c07c18e
Fedor Zhilkin <fzhilkin@mail.ru>
2023-10-22 02:48:24 +0300

    Downgrade speaker diarization model version

--- predict.py
+++ predict.py
@@ -41,12 +41,12 @@ class Predictor(BasePredictor):
             elif lang == 'ru':
                 result = whisperx.align(result['segments'], self.allign_model_ru, self.metadata_ru, audio, self.device, return_char_alignments=False)
             else:
-                model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=self.device)
+                model_a, metadata = whisperx.load_align_model(language_code=lang, device=self.device)
                 result = whisperx.align(result['segments'], model_a, metadata, audio, self.device, return_char_alignments=False)
 
             # 3. Assign speaker labels
             if hugging_face_token:
-                diarize_model = whisperx.DiarizationPipeline(use_auth_token=hugging_face_token, device=self.device)
+                diarize_model = whisperx.DiarizationPipeline(model_name='pyannote/speaker-diarization@2.1', use_auth_token=hugging_face_token, device=self.device)
                 diarize_segments = diarize_model(audio)
                 result = whisperx.assign_word_speakers(diarize_segments, result)
 
